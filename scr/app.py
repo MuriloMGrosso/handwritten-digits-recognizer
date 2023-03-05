70ec2bfa7decb90c20cbac834e6db24c78502bb1
Murilo M. Grosso <murilogrosso@hotmail.com>
2023-03-05 06:14:13 -0300

    window update

--- scr/app.py
+++ scr/app.py
@@ -4,8 +4,8 @@ import neural_network as neural
 
 class Drawing_Canvas():
     def __init__(self, canvas_size, grid_size):
-        self.background_color = '#ffffff'
-        self.main_color = '#000000'
+        self.background_color = '#E5FCC2'
+        self.main_color = '#111111'
 
         self.canvas = tk.Canvas(root, bg=self.background_color)
         self.canvas.place(width=canvas_size, height=canvas_size)
@@ -24,14 +24,14 @@ class Drawing_Canvas():
         if x < self.grid_size and y < self.grid_size:
             self.set_pixel(x, y, self.main_color)
             self.grid[int(y * self.grid_size + x)] = [255]
-            self.evaluate()
+            evaluate(self.grid)
 
     def erase(self, event):
         x, y = self.canvas_to_grid(event)
         if x < self.grid_size and y < self.grid_size:
             self.set_pixel(x, y, self.background_color)
             self.grid[int(y * self.grid_size + x)] = [0]
-            self.evaluate()
+            evaluate(self.grid)
 
     def canvas_to_grid(self, event):
         x, y = event.x, event.y
@@ -42,39 +42,50 @@ class Drawing_Canvas():
         fixed_x, fixed_y = x * self.canvas_per_grid, y * self.canvas_per_grid
         self.canvas.create_rectangle(fixed_x, fixed_y, fixed_x + self.canvas_per_grid, fixed_y + self.canvas_per_grid, fill=color, width=0)
 
-    def evaluate(self):
-        output = net.evaluate(self.grid)
-        stats_canvas.update_bars(output)
+class Bar():
+    def __init__(self, canvas, color, bg_color, border, canvas_height, canvas_width, offset):
+        self.canvas = canvas
+        label_space = 32
+        self.width = canvas_width - 2*border - label_space
+        height = (canvas_height - border) / 10 - border
+
+        self.x0 = border + label_space
+        self.y0 = offset * (border + height) + border
+        self.x1 = border + label_space + self.width
+        self.y1 = (offset + 1) * (border + height)
+
+        self.canvas.create_rectangle(self.x0,self.y0,self.x1,self.y1,fill=bg_color, width=0)
+        self.bar = self.canvas.create_rectangle(self.x0,self.y0,self.x1 - self.width,self.y1,fill=color, width=0)
+        self.text = self.canvas.create_text((self.x0 + self.x1)/2, (self.y0 + self.y1)/2, text='0%', fill='#E5FCC2', font=("Arial", 16))
+        self.canvas.create_text(self.x0/2, (self.y0 + self.y1)/2, text=f'{offset}', fill='#E5FCC2', font=("Arial", 16))
+
+    def update(self, scale):
+        self.canvas.coords(self.bar, self.x0, self.y0, self.x1 - (1 - scale) * self.width, self.y1)
+        self.canvas.itemconfig(self.text, text=f'{round(scale*100)}%')
 
 class Stats_Canvas():
     def __init__(self):
-        self.width, self.height = 204, 308
-        self.canvas = tk.Canvas(root, bg='#000000')
+        self.width, self.height = 204, 512
+        self.canvas = tk.Canvas(root, bg='#111111')
         self.canvas.place(width=self.width, height=self.height, relx=1.0, rely=0.0, anchor='ne')
 
         num_bars = 10
         self.bars = []
         for i in range(num_bars):
-            bar = self.canvas.create_rectangle(0,0,0,0,fill='#555555', width=0)
-            self.update_bar(bar,1,i)
-        for i in range(num_bars):
-            bar = self.canvas.create_rectangle(0,0,0,0,fill='#ffffff', width=0)
-            self.update_bar(bar,0,i)
-            self.bars.append(bar)
-
-    def update_bar(self, bar, scale, offset):
-        border = 16
-        bar_height = (self.height - 11 * border) / 10
-        bar_width = self.width - 2 * border
-
-        self.canvas.coords(bar, border, offset * (border + bar_height) + border, border + scale * bar_width, (offset + 1) * (border + bar_height))
+            self.bars.append(Bar(self.canvas, '#547980', '#594F4F', 16, self.height, self.width, i))
 
     def update_bars(self,scales):
         for i in range(len(self.bars)):
-            self.update_bar(self.bars[i],scales[i][0],i)
+            self.bars[i].update(scales[i][0])
+
+def evaluate(data):
+    output = net.evaluate(data)
+    stats_canvas.update_bars(output)
+    label.config(text=f"{np.argmax(output)}")
 
 root = tk.Tk()
 root.geometry('512x512')
+root.config(bg='#45ADA8')
 root.resizable(False, False)
 root.title('Neural Network')
 
@@ -83,5 +94,7 @@ net.load('../network_data/DigitsRecognizer.npy')
 
 stats_canvas = Stats_Canvas()
 drawing_canvas = Drawing_Canvas(308,28)
+label = tk.Label(root, text="?", font=("Arial", 128), fg='#E5FCC2', bg='#45ADA8')
+label.place(x=100, y=310)
 
 root.mainloop()
