0cf7a282f0c29b079c59090c652958ccbfc140bc
Murilo M. Grosso <murilogrosso@hotmail.com>
2023-05-04 16:20:18 -0300

    requirements

--- scr/train_network.py
+++
@@ -1,9 +0,0 @@
-import neural_network as neural
-import data_manager
-
-net = neural.Network()
-#net = neural.Network([784,128,30,10], "DigitsRecognizer0.2")
-net.load("DigitsRecognizer0.2")
-
-training_data, test_data = data_manager.get_data()
-net.train(training_data, 480, 100, 3.0, test_data)
